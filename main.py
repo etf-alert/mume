634fa842ed9a87de5eac92345d390facaf2766dd
etf-alert <kdj12358@gmail.com>
2026-02-01 01:31:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -169,10 +169,13 @@ def cron_save(secret: str = Query(...)):
 # =====================
 # API
 # =====================
-@app.get("/")
-def root():
-    return {"status": "ok"}
 
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
 @app.get("/tickers")
 def get_tickers():
