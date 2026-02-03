4b4a3cd40a64bc43164b1bdae5cdfad78ee32491
etf-alert <kdj12358@gmail.com>
2026-02-03 23:38:08 +0900

    Update market_time.py

--- market_time.py
+++ market_time.py
@@ -5,6 +5,38 @@ import pytz
 nyse = mcal.get_calendar("NYSE")
 ny_tz = pytz.timezone("US/Eastern")
 
+def is_us_premarket(now=None):
+    if not now:
+        now = datetime.now(ny_tz)
+
+    schedule = nyse.schedule(
+        start_date=now.date(),
+        end_date=now.date()
+    )
+    if schedule.empty:
+        return False
+
+    open_time = schedule.iloc[0]["market_open"].to_pydatetime()
+    pre_open = open_time.replace(hour=4, minute=0)
+
+    return pre_open <= now < open_time
+
+
+def is_us_postmarket(now=None):
+    if not now:
+        now = datetime.now(ny_tz)
+
+    schedule = nyse.schedule(
+        start_date=now.date(),
+        end_date=now.date()
+    )
+    if schedule.empty:
+        return False
+
+    close_time = schedule.iloc[0]["market_close"].to_pydatetime()
+    post_close = close_time.replace(hour=20, minute=0)
+
+    return close_time < now <= post_close
 
 def is_us_market_open(now=None):
     if not now:
